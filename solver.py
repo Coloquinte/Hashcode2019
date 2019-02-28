7df59126bc3a2a19ef22f34f9dfc1d3694421b8e
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2019-02-28 20:11:06 +0100

    Basic TSP code

--- solver.py
+++ solver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import sys
+import networkx
+import localsolver
 
 def parse_file(filename):
     with open(filename) as f:
@@ -49,6 +51,35 @@ def make_paired_solution(tags, horizontal):
     himgs = [i for i, h in enumerate(horizontal) if h]
     return [(i,) for i in himgs] + [(vimgs[2*j], vimgs[2*j+1]) for j in range(int(len(vimgs)/2))]
 
+def make_paired_solution_matching(tags, horizontal):
+    vimgs = [i for i, h in enumerate(horizontal) if not h]
+    himgs = [i for i, h in enumerate(horizontal) if h]
+    vslides = [(vimgs[2*j], vimgs[2*j+1]) for j in range(int(len(vimgs)/2))]
+    sum_common = sum(len(tags[v[0]] & tags[v[1]]) for v in vslides)
+    #print("Common", sum_common)
+    return [(i,) for i in himgs] + vslides
+
+def optimize(tags, horizontal, sol, start, end):
+    subtour = sol[start:end]
+    nb_cities = len(subtour)
+    sol_tags = [slide_tags(tags, s) for s in subtour]
+    distance_weight = [[cost_between_tags(sol_tags[i], sol_tags[j]) for i in range(nb_cities)] for j in range(nb_cities)]
+    with localsolver.LocalSolver() as ls:
+        model = ls.model
+        cities = model.list(end - start)
+        model.constraint(model.count(cities) == end-start)
+        distance_array = model.array(distance_weight)
+        dist_selector = model.function(lambda i: model.at(distance_array, cities[i-1], cities[i]))
+        obj = model.sum(model.range(1, nb_cities), dist_selector)
+        model.maximize(obj)
+        model.close()
+        ls.param.time_limit = 10
+        for c in range(nb_cities):
+          cities.value.add(c)
+        ls.solve()
+        new_subtour = [subtour[c] for c in cities.value]
+        sol[start:end] = new_subtour
+
 if len(sys.argv) < 2:
   print("solver.py input [output]")
   sys.exit(1)
@@ -57,8 +88,16 @@ tags, horizontal = parse_file(sys.argv[1])
 
 sol = [(i,) for i in range(len(tags))]
 sol = make_paired_solution(tags, horizontal)
+#sol = make_paired_solution_matching(tags, horizontal)
 
 print (sol_cost(tags, sol))
+
+subsize = 500
+for i in range(10):
+    if (i+1) * subsize > len(sol):
+      break
+    optimize(tags, horizontal, sol, i * subsize, min( (i+1) * subsize, len(sol)))
+    print (sol_cost(tags, sol))
 
 if len(sys.argv) >= 3:
   write_solution(sol, sys.argv[2])
